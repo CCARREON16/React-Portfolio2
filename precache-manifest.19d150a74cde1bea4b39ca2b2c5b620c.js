a76fca02ec976c1414750e5793b7ea091329a1ae
Chris Carreon <mrwiggles6545@yahoo.com>
2020-09-08 22:25:06 -0500

    Updates

---
+++ precache-manifest.19d150a74cde1bea4b39ca2b2c5b620c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d53c61cddfeac1a8f914aeb9011a7b10",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "f1599249d3713e923be4",
+    "url": "/React-Portfolio/static/css/main.4283dff0.chunk.css"
+  },
+  {
+    "revision": "d7f3db5d54b911d3f75b",
+    "url": "/React-Portfolio/static/js/2.77950bab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Portfolio/static/js/2.77950bab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1599249d3713e923be4",
+    "url": "/React-Portfolio/static/js/main.2da00ba6.chunk.js"
+  },
+  {
+    "revision": "0274f599536680666069",
+    "url": "/React-Portfolio/static/js/runtime-main.27e0f504.js"
+  }
+]);

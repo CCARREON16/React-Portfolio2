89a495fb609d0cdf04c0c79d64c98b77025d0ca3
Chris Carreon <mrwiggles6545@yahoo.com>
2020-09-08 22:24:16 -0500

    made changes due to repo not working

---
+++ src/Portfolio.js
@@ -0,0 +1,86 @@
+export default {
+
+    name: "",
+    headerTagline: [
+      //Line 1 For Header
+      "Chrisopher James Carreon Portfolio",
+      //Line 2 For Header
+      "Full Stack web developer ",
+      //Line 3 For Header
+      "from San Antonio, Texas",
+    ],
+    //Contact Email
+    contactEmail: "mrwiggles6545@yahoo.com",
+    // About Me
+    abouttext:
+      "Hello, my name is Christopher James Carreon. I am 31 years old and a Web developer and an aspiring Web Architect.",
+    aboutImage:
+      "https://i.imgur.com/lohd0G3.png[/img]",
+    
+    ShowAboutImage: true, 
+    // Change Projects Here
+    projects: [
+      {
+        id: 1, 
+        title: "Brewery Project", 
+        service: "", 
+        imageSrc:
+       "https://i.imgur.com/69z1PhA.png[/img]",
+       //Project URL 
+       url: "https://ccarreon16.github.io/BreweryProject/", //first [roject i did
+        
+       
+        
+      },
+      {
+        id: 2, 
+        title: "Password Genie",
+        service: "",
+        imageSrc:
+       "https://i.imgur.com/dkKV5cP.png[/img]",
+         
+       
+       
+       
+       url: "https://arcane-inlet-71435.herokuapp.com/",///first project i did with team
+      },
+      {
+        id: 3, 
+        title: "Employee Tracker",
+        service: "",
+        imageSrc:
+        "https://i.imgur.com/3o5zYul.png[/img]",
+       url: "https://github.com/CCARREON16/EmployeeTracker-",
+      },
+      {
+        id: 4, 
+        title: "Eat-Da-Burger",
+        service: "",
+        imageSrc:
+        "https://i.imgur.com/pZP6JXk.png[/img]",
+        
+        url: "https://stormy-woodland-63462.herokuapp.com/",
+      },
+      {
+        id: 5, 
+        title: "Online-Offline-Budget-Trackers",
+        service: "",
+        imageSrc:
+        "https://i.imgur.com/xXeD7PK.png[/img]",
+        
+        url: "https://ancient-crag-17293.herokuapp.com/",
+      },
+    ],
+    social: [
+      // Links for github, linkedin and resume
+      {name: "Github", url: "https://github.com/CCARREON16"},
+      {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/christopher-carreon-88342087/",
+      },
+      {
+        name: "Resume",
+        url: "https://www.github.com/CCARREON16/UpdatedPortfolio/blob/master/assets/Chris%20Careon%20Resume2020%20(1)%20(1).pdf",
+      },
+    ],
+  };
